17e4cd93ac78bc651f9a888c386feee00e747b8c
Inna Osipova <rutheva_inna@mail.ru>
2022-11-10 19:24:40 +0300

    Сумма Deque  двумя способами

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -13,9 +13,10 @@ public class Task1 {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
         Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(3,2,1));
-        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(7,4,5));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(7,5,7));
         Logger logger = Logger.getLogger(Task1.class.getName());
-        logger.info(String.valueOf(task1.sum(d1, d2)));
+        //logger.info(String.valueOf(task1.sum(d1, d2)));
+        logger.info(String.valueOf(task1.sum2(d1, d2)));
         // result [1,0,6,6]
 
     }
@@ -38,6 +39,30 @@ public class Task1 {
             number = number/10;
         }deque.addFirst(number%10);
 
+        return deque;
+    }
+    
+    public Deque<Integer> sum2(Deque<Integer> d1, Deque<Integer> d2){
+        Deque<Integer> deque = new ArrayDeque<>();
+        int max = d1.size();
+        if (max < d2.size()) max = d2.size(); 
+        int max1 = max;
+        int k = 0;
+        while (max > 0){
+            if(d1.size() == 0) d1.add(0);
+            if(d2.size() == 0) d2.add(0);
+            int sum = d1.pollLast() + d2.pollLast() + k;
+            System.out.println(sum);
+            if(sum < 10 ){
+                deque.addFirst(sum);
+                max--;
+            }else{
+                deque.addFirst(sum%10);
+                k = sum/10;
+                max--;
+            }
+        if (k != 0 && deque.size() <= max1+1) deque.addFirst(k);
+        }
         return deque;
     }
 }
