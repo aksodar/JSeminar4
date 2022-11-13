53191a9832fc7ddf7cabfa58258ca9e4babd2c7d
MaxLeontev <lmaks21@mail.ru>
2022-11-13 12:10:22 +0300

    add  task

---
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -0,0 +1,37 @@
+package ru.gb.jseminar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeworkTest {
+
+    @org.junit.jupiter.api.Test
+    void multipleText() {
+        Deque<Integer> expectedList1 = new ArrayDeque<>(Arrays.asList(5,2));
+        Deque<Integer>expectedList2 = new ArrayDeque<>(Arrays.asList(5,2));
+        Deque<Integer>expected = new ArrayDeque<>(Arrays.asList(5,2,6));
+
+        Homework homework = new Homework();
+        Deque<Integer>actual = homework.multiple(expectedList1,expectedList2);
+
+        Assertions.assertIterableEquals(expected,actual);
+    }
+
+    @Test
+    void sum() {
+        Deque<Integer> expectedList1 = new ArrayDeque<>(Arrays.asList(5,2));
+        Deque<Integer>expectedList2 = new ArrayDeque<>(Arrays.asList(5,2));
+        Deque<Integer>expected = new ArrayDeque<>(Arrays.asList(0,5));
+
+        Homework homework = new Homework();
+        Deque<Integer>actual = homework.sum(expectedList1,expectedList2);
+
+        Assertions.assertIterableEquals(expected,actual);
+    }
+}
