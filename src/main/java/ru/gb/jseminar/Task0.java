e66df9dc0d88358e47bdc99adb93a3a212798b4f
oleg <olegarxo15@gmail.com>
2022-11-14 16:51:56 +0300

    Class work

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,5 +1,6 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
@@ -9,13 +10,25 @@ public class Task0 {
     // Дан Deque состоящий из последовательности цифр.
     // Необходимо проверить, что последовательность цифр является палиндромом
     public static void main(String[] args) {
-        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
+        Task0 task0 = new Task0();
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,3,2,1));
+        Logger log = Logger.getLogger(Task0.class.getName());
+        log.info(String.valueOf(task0.checkOn(deque)));
+        
 
     }
 
 
     public boolean checkOn(Deque<Integer> deque){
-
-        return false;
+        while (deque.size() > 1){
+            if (deque.getFirst() == deque.getLast()){
+                deque.pollFirst();
+                deque.pollLast();
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
