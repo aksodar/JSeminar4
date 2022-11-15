5722c1fb5b6a5e62d46688e6fa03089e59b21f1b
Nikita <smiels@vk.com>
2022-11-15 19:47:23 +0300

    add hw task

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -3,6 +3,7 @@ package ru.gb.jseminar;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.logging.Logger;
 
 public class Homework {
 
@@ -10,21 +11,58 @@ public class Homework {
     // и каждый из их узлов содержит одну цифру.
     public static void main(String[] args) {
         Homework hw = new Homework();
-        hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)));
-        // result [0,0,1]
-        hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)));
-        // result [0,-2]
+        Logger log = Logger.getLogger(Homework.class.getName());
+        log.info(String.valueOf(hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)))));
+        log.info(String.valueOf(hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)))));
+
+//        hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)));
+//        // result [0,0,1]
+//        hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)));
+//        // result [0,-2]
     }
 
     // Умножьте два числа и верните произведение в виде связанного списка.
-    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2){
-
-        return new ArrayDeque<>();
+    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+        int intResult = 0;
+        int temp = 0;
+        while (d1.peekFirst() != null) {
+            num1.append(d1.pollLast().toString());
+        }
+        while (d2.peekFirst() != null) {
+            num2.append(d2.pollLast().toString());
+        }
+        intResult = Integer.parseInt(String.valueOf(num1)) * Integer.parseInt(String.valueOf(num2));
+        while (intResult != 0) {
+            temp = intResult % 10;
+            intResult /= 10;
+            result.addLast(temp);
+        }
+        return result;
     }
 
     // Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+        int intResult = 0;
+        int temp = 0;
+        while (d1.peekFirst() != null) {
+            num1.append(d1.pollLast().toString());
+        }
+        while (d2.peekFirst() != null) {
+            num2.append(d2.pollLast().toString());
+        }
+        intResult = Integer.parseInt(String.valueOf(num1)) + Integer.parseInt(String.valueOf(num2));
+            while (intResult != 0) {
+                temp = intResult % 10;
+                intResult /= 10;
+                result.addLast(temp);
+            }
 
-        return new ArrayDeque<>();
+        return result;
     }
 }
