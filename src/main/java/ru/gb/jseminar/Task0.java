52b63b454cb433c4d7cc8c7e73afbea8e3575456
Rendo-M <srvr@list.ru>
2022-12-17 20:15:42 +0300

    homework was added

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -8,14 +8,22 @@ public class Task0 {
 
     // Дан Deque состоящий из последовательности цифр.
     // Необходимо проверить, что последовательность цифр является палиндромом
-    public static void main(String[] args) {
-        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
-
+    public static void main(String[] args) throws Exception {
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,3,2,1));
+        Task0 first = new Task0();
+        
+            System.out.println(first.checkOn(deque));
     }
+        
 
 
-    public boolean checkOn(Deque<Integer> deque){
-
-        return false;
+    public boolean checkOn(Deque<Integer> deque) throws Exception {
+        boolean result = true;
+        if (deque == null) throw new Exception("Коллекция ссылается на null");
+        while (result && deque.size()>1)
+        {
+            result = result && (deque.pollFirst().equals(deque.pollLast()));
+        }
+        return result;
     }
 }
