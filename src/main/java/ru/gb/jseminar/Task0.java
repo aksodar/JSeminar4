8c0b656a21550b299bd92b833f801e63b9b7f07d
MDementev <vanneste1207@gmail.com>
2022-12-21 21:26:29 +0300

    seminar4

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,21 +1,25 @@
-package ru.gb.jseminar;
-
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+package JSeminar_4.src.main.java.ru.gb.jseminar;
+import java.util.*;
 
 public class Task0 {
-
-    // Дан Deque состоящий из последовательности цифр.
-    // Необходимо проверить, что последовательность цифр является палиндромом
-    public static void main(String[] args) {
-        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
-
+    // Дан Deque состоящий из последовательности цифр
+    // Необходимо проверить, что последовательность цифр является полиндромом.
+    public static void main(String[] args) throws Exception {
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,2,1));
+        Task0 polindrom = new Task0();
+        System.out.println(polindrom.checkOn(deque)); 
     }
 
-
-    public boolean checkOn(Deque<Integer> deque){
-
-        return false;
+    public boolean checkOn(Deque<Integer> deque) throws Exception {
+        if(deque == null || deque.size() == 0){
+            System.out.println("");
+            throw new Exception("Коллекция пустая");
+        }
+        while(deque.size()>1){
+            if(!deque.pollFirst().equals(deque.pollLast())){
+                return false;
+            }            
+        }
+        return true;
     }
 }
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,24 +1,44 @@
-package ru.gb.jseminar;
+package JSeminar_4.src.main.java.ru.gb.jseminar;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
 
 public class Task1 {
 
-    //Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
+    // Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся
+    // в обратном порядке
     // и каждый из их узлов содержит одну цифру.
     // Сложите два числа и верните сумму в виде связанного списка.
-    public static void main(String[] args) {
-        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,2,3));
-        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,4,7));
-        // result [6,6,0,1]
-
+    public static void main(String[] args) throws Exception {
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1, 5, 2, 3));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5, 4, 7, 7));
+        Task1 sq = new Task1();
+        System.out.println("Сумма = " + sq.sum(d1, d2).toString());
     }
 
-
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
-
-        return new ArrayDeque<>();
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Входные данные не заданы");
+        }
+        Deque<Integer> result = new ArrayDeque<>();
+        int doz = 0;
+        while (d1.size() > 0 || d2.size() > 0) {
+            int sum = doz;
+            doz = 0;
+            if (d1.size() > 0) {
+                sum += d1.pollFirst();
+            }
+            if (d2.size() > 0) {
+                sum += d2.pollFirst();
+            }
+            if (sum >= 10) {
+                sum = sum - 10;
+                doz = 1;
+            }
+            result.offer(sum);
+        }
+        if (doz != 0) {
+            result.offer(doz);
+        }
+        return result;
     }
 }
