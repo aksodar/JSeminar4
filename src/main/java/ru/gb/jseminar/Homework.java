4397970c33fbdb1977b4306e1268e917f9485e6a
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-12-21 22:02:04 +0700

    Peremyshlennikov GB 3485

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -6,25 +6,123 @@ import java.util.Deque;
 
 public class Homework {
 
-    //Даны два Deque представляющие два целых числа. Цифры хранятся в обратном порядке,
+    // Даны два Deque представляющие два целых числа. Цифры хранятся в обратном
+    // порядке,
     // и каждый из их узлов содержит одну цифру.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Homework hw = new Homework();
-        hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)));
+        System.out.println("Произведение: "
+                + hw.multiple(new ArrayDeque<>(Arrays.asList(5, 5)), new ArrayDeque<>(Arrays.asList(5, 3))));
         // result [0,0,1]
-        hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)));
+        System.out.println("Разница: "
+                + hw.difference(new ArrayDeque<>(Arrays.asList( 3,2,1)), new ArrayDeque<>(Arrays.asList(5, 1))));
         // result [0,-2]
     }
 
     // Умножьте два числа и верните произведение в виде связанного списка.
-    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Входные данные не заданы");
+        }
+        Deque<Integer> result = new ArrayDeque<>(0);
+        Deque<Integer> line = new ArrayDeque<>(result);
+        int multiplier;
+        int mult = 1;
+        int rank = 0;
+        int count = 0;
 
-        return new ArrayDeque<>();
+        while (!d2.isEmpty()) {
+            Deque<Integer> tmpDeque = new ArrayDeque<>(d1);
+            multiplier = d2.poll();
+            while (!tmpDeque.isEmpty()) {
+                mult = tmpDeque.poll() * multiplier + rank;
+                rank = 0;
+                if (mult >= 10) {
+                    rank = mult / 10;
+                    mult = mult % 10;
+                }
+                line.offer(mult);
+            }
+            if (rank != 0) {
+                line.offer(rank);
+
+            }
+            rank = 0;
+            for (int index = 0; index < count; index++) {
+                line.addFirst(0);
+            }
+            count++;
+
+            Homework plus = new Homework();
+            result = plus.sum(line, result);
+
+        }
+
+        return result;
     }
 
-    // Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Входные данные не заданы");
+        }
+        Deque<Integer> result = new ArrayDeque<>();
+        int digit = 0;
+        while (d1.size() > 0 || d2.size() > 0) {
+            int sum = digit;
+            digit = 0;
+            if (d1.size() > 0) {
+                sum += d1.pollFirst();
+            }
+            if (d2.size() > 0) {
+                sum += d2.pollFirst();
+            }
+            if (sum >= 10) {
+                sum -= 10;
+                digit = 1;
+            }
 
-        return new ArrayDeque<>();
+            result.offer(sum);
+        }
+        if (digit != 0) {
+            result.offer(digit);
+        }
+
+        return result;
+    }
+
+    // Сложите два числа и верните сумму в виде связанного списка. Одно или два
+    // числа должны быть отрицательными
+
+    //вычитание второго числа из первого
+    public Deque<Integer> difference(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Входные данные не заданы");
+        }
+        Deque<Integer> result = new ArrayDeque<>();
+        int digit = 0;
+        int digit1;
+        int digit2;
+        while (d1.size() > 0 || d2.size() > 0) {
+            int res = 0;
+            if (d1.size() > 0) {
+                digit1 = d1.pollFirst() - digit;
+            }
+            else{ digit1 = 0;}
+            if (d2.size() > 0) {
+                digit2 = d2.pollFirst();
+            }
+            else{ digit2 = 0;}
+            digit = 0;
+            if (digit1 < digit2) {
+                res = digit1 + 10 - digit2;
+                digit = 1;
+            } else {
+                res = digit1 - digit2;
+            }
+
+            result.offer(res);
+        }
+
+        return result;
     }
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,14 +1,19 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Task2 {
 
-    //Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', определите,
+    // Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']',
+    // определите,
     // является ли входная строка логически правильной.
     // Входная строка логически правильная, если:
     // 1) Открытые скобки должны быть закрыты скобками того же типа.
-    // 2) Открытые скобки должны быть закрыты в правильном порядке. Каждая закрывающая скобка имеет соответствующую
+    // 2) Открытые скобки должны быть закрыты в правильном порядке. Каждая
+    // закрывающая скобка имеет соответствующую
     // открытую скобку того же типа.
     // ()[] = true
     // () = true
@@ -17,11 +22,31 @@ public class Task2 {
     // )()( = false
 
     public static void main(String[] args) {
-
+        Task2 obj = new Task2();
+        System.out.println(obj.validate("()[]"));
+        System.out.println(obj.validate("()"));
+        System.out.println(obj.validate("{[()]}"));
+        System.out.println(obj.validate("()()"));
+        System.out.println(obj.validate(")()("));
     }
 
-    public boolean validate(Deque<Integer> deque){
+    public boolean validate(String inputString) {
+        Map<Character, Character> bracketsMap = new Hashtable<Character, Character>();
+        bracketsMap.put(')', '(');
+        bracketsMap.put(']', '[');
+        bracketsMap.put('}', '{');
+        Deque<Character> result = new ArrayDeque<>();
 
-        return false;
+        for (char bracket : inputString.toCharArray()) {
+            if (bracketsMap.containsValue(bracket)) {
+                result.addFirst(bracket);
+            } 
+            else if (bracketsMap.containsKey(bracket)) {
+                if (result.isEmpty() || result.pollFirst() != bracketsMap.get(bracket)) {
+                    return false;
+                }
+            }
+        }
+        return result.isEmpty();
     }
 }
