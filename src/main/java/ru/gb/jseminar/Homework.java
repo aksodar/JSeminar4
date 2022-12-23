9978e33eea0d6a41d1161269e5a380099f4a5c46
Vera Lippgardt <110783736+Vera1078@users.noreply.github.com>
2022-12-23 18:56:05 +0300

    HW 4

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,30 +1,77 @@
 package ru.gb.jseminar;
 
+import java.util.Collection;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.Deque;
+import java.util.stream.Collectors;
+
 
 public class Homework {
 
     //Даны два Deque представляющие два целых числа. Цифры хранятся в обратном порядке,
     // и каждый из их узлов содержит одну цифру.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Homework hw = new Homework();
-        hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)));
+        hw.multiple(new ArrayDeque<>(Arrays.asList(5, 2)), new ArrayDeque<>(Arrays.asList(4)));
         // result [0,0,1]
         hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)));
         // result [0,-2]
     }
 
     // Умножьте два числа и верните произведение в виде связанного списка.
-    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        Deque<Integer> res = new ArrayDeque<>();
 
-        return new ArrayDeque<>();
+        if (d1 == null || d2 == null) {
+            throw new Exception("Входные данные отсутсвуют!");
+        }
+
+        int value = -1;
+        int rank = 0;
+        while (d1.size() > 0) {
+            value = (d1.pollFirst() * d2.getFirst()) + rank;
+            if (value >= 10) {
+                rank = value / 10;
+                value = 0;
+            }
+            res.offer(value);
+        }
+        if (rank != 0) {
+            res.offer(rank);
+        }
+        System.out.println("Функция 'multiple': " + res);
+        return res;
     }
 
     // Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        Deque<Integer> res = new ArrayDeque<>();
 
-        return new ArrayDeque<>();
+        if (d1 == null || d2 == null) {
+            throw new Exception("Данные отсутсвуют.");
+        }
+        // Конвертирование списка в число в обратном порядке
+        StringBuilder s1 = new StringBuilder();
+        for (int i = 0; i < d1.size(); i++) {
+            s1.append(d1.pollLast());
+            i--;
+        }
+        int number_d1 = Integer.parseInt(s1.toString());
+        int rank = 0;
+        int value = number_d1 + d2.getFirst();
+
+        if (value >= 10 || value <= -10) {
+            rank = value / 10;
+            value = 0;
+        }
+        res.offer(value);
+        if (rank != 0) {
+            res.offer(rank);
+        }
+
+        System.out.println("Функция 'sum': " + res);
+        return res;
     }
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,8 +1,14 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Stack;
 
 public class Task2 {
+
 
     //Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', определите,
     // является ли входная строка логически правильной.
@@ -17,11 +23,28 @@ public class Task2 {
     // )()( = false
 
     public static void main(String[] args) {
-
+        Task2 signs = new Task2();
+        Deque<Character> stringSymbols = new ArrayDeque<>(Arrays.asList('{','(','{','}',')','[',']','}'));
+        System.out.println(signs.validate(stringSymbols));
     }
 
-    public boolean validate(Deque<Integer> deque){
 
-        return false;
+    public boolean validate(Deque<Character> deque){
+        Map<Character, Character> val = new Hashtable<Character, Character>();
+        val.put(']', '[');
+        val.put(')', '(');
+        val.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        char element;
+        while (deque.size() > 0){
+            element = deque.poll();
+            if (val.containsValue(element))
+                stack.push(element);
+            else
+            if (val.containsKey(element))
+                if (stack.size() == 0 || stack.pop() != val.get(element)) return false;
+
+        }
+        return stack.size() == 0;
     }
 }
