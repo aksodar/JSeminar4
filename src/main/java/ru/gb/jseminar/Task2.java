32a54086b288557d06e1a9063171fe98f98d1b3a
anatoly <tolyan.yalta@yandex.ru>
2022-12-24 23:03:17 +0300

    Add file Task2.java

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,5 +1,8 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Deque;
 
 public class Task2 {
@@ -16,12 +19,87 @@ public class Task2 {
     // ()() = true
     // )()( = false
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String str = "(){{[()]}()}()";
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] c = str.toCharArray();
 
+        for (char i: c) {
+            deque.offerLast(i);
+        }
+        System.out.println(deque);
+
+        //Deque<Character> deque = new ArrayDeque<>(Arrays.asList('(', '{', '[', '(', ')', ']', '}', '[', ']', ')'));
+        //Deque<Character> deque = new ArrayDeque<>(Arrays.asList('{','[','(',')',']','}'));
+        Task2 result = new Task2();
+        System.out.println(result.validate(deque));
     }
 
-    public boolean validate(Deque<Integer> deque){
-
-        return false;
+    public boolean validate(Deque<Character> deque) throws Exception {
+        if (deque.size() % 2 != 0) {
+            throw new Exception("Количество символов не четное!");
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        Character temp;
+        //Character tempLast;
+        while (deque.size() > 0) {
+            temp = deque.pollFirst();
+            switch (temp) {
+                case 123:   // { - 123
+                    if (deque.size() == 0){
+                        break;
+                    } else if (deque.peekFirst() == 125) {
+                        deque.pollFirst();
+                    } else {
+                        stack.offerFirst(temp);
+                    }
+                    break;
+                case 125:   // } - 125
+                    if (stack.size() > 0 && (stack.peekFirst() == 123)) {
+                        stack.pollFirst();
+                        break;
+                    }
+                    stack.offerFirst(temp);
+                    break;
+                case 91:    // [ - 91
+                    if (deque.size() == 0){
+                        break;
+                    } else if (deque.peekFirst() == 93) {
+                        deque.pollFirst();
+                    } else {
+                        stack.offerFirst(temp);
+                    }
+                    break;
+                case 93:    // ] - 93
+                    if (stack.size() > 0 && (stack.peekFirst() == 91)) {
+                        stack.pollFirst();
+                        break;
+                    }
+                    stack.offerFirst(temp);
+                    break;
+                case 40:    // ( - 40
+                    if (deque.size() == 0){
+                        break;
+                    } else if (deque.peekFirst() == 41) {
+                        deque.pollFirst();
+                    } else {
+                        stack.offerFirst(temp);
+                    }
+                    break;
+                case 41:    // ) - 41
+                    if (stack.size() > 0 && stack.peekFirst() == 40) {
+                        stack.pollFirst();
+                        break;
+                    }
+                    stack.offerFirst(temp);
+                    break;
+            }
+        }
+        if (stack.size() > 0) {
+            return false;
+        }
+        return true;
     }
 }
+
+
