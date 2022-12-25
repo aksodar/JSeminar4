8aceac09d33be76b0c8c74ed1a744ed4dc4b456c
MuxauJI <Grishin.MixauJl@yandex.ru>
2022-12-25 13:37:41 +0300

    Task2 and HW are ready for review.

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,5 +1,6 @@
 package ru.gb.jseminar;
 
+import java.net.StandardSocketOptions;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
@@ -8,23 +9,75 @@ public class Homework {
 
     //Даны два Deque представляющие два целых числа. Цифры хранятся в обратном порядке,
     // и каждый из их узлов содержит одну цифру.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Homework hw = new Homework();
-        hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4)));
+        //System.out.println("The result is  " + hw.multiple(new ArrayDeque<>(Arrays.asList(5,2)), new ArrayDeque<>(Arrays.asList(4))));
         // result [0,0,1]
-        hw.sum(new ArrayDeque<>(Arrays.asList(5,-2)), new ArrayDeque<>(Arrays.asList(5)));
+        //System.out.println("The result is  " + hw.sum(new ArrayDeque<>(Arrays.asList(8,3,-1)), new ArrayDeque<>(Arrays.asList(-8))));
         // result [0,-2]
     }
 
     // Умножьте два числа и верните произведение в виде связанного списка.
-    public Deque<Integer> multiple(Deque<Integer> d1, Deque<Integer> d2){
+    public Deque<Character> multiple(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Empty data");
+        }
+        ArrayDeque<Character> res = new ArrayDeque<>();
+        String first = "";
+        String scnd = "";
+        while (d1.size() > 0){
+            String str1 = String.valueOf(d1.pollLast());
+            first = first + str1;
+        }
+        System.out.println("First value - " + first);
+        int xx = Integer.parseInt(first.trim());
+        while (d2.size() > 0){
+            String str2 = String.valueOf(d2.pollLast());
+            scnd = scnd + str2;
+        }
+        System.out.println("Second value - " + scnd);
+        int yy = Integer.parseInt(scnd.trim());
+        int mult = xx * yy;
+        String multStr = String.valueOf(mult);
+        char[] chars = multStr.toCharArray();
 
-        return new ArrayDeque<>();
+        for (char ch: chars){
+            res.addFirst(ch);
+        }
+        return res;
     }
 
     // Сложите два числа и верните сумму в виде связанного списка. Одно или два числа должны быть отрицательными
-    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2){
+    //Последний знак минус указывает на отрицательную сумму двух чисел.
+    public Deque<Character> sum(Deque<Integer> d1, Deque<Integer> d2) throws Exception {
+        if (d1 == null || d2 == null) {
+            throw new Exception("Empty data");
+        }
+        ArrayDeque<Character> res = new ArrayDeque<>();
+        String first = "";
+        String scnd = "";
+        while (d1.size() > 0){
+            String str1 = String.valueOf(d1.pollLast());
+            first = first + str1;
+        }
+        System.out.println("First value: " + first);
+        int xx = Integer.parseInt(first.trim());
+        while (d2.size() > 0){
+            String str2 = String.valueOf(d2.pollLast());
+            scnd = scnd + str2;
+        }
+        System.out.println("Second value: " + scnd);
+        int yy = Integer.parseInt(scnd.trim());
+        int mult = xx + yy;
+        if (mult > 0){
 
-        return new ArrayDeque<>();
+        }
+        String multStr = String.valueOf(mult);
+        char[] chars = multStr.toCharArray();
+
+        for (char ch: chars){
+            res.addFirst(ch);
+        }
+        return res;
     }
 }
