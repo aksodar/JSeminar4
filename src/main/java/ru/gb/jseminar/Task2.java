40ee8312ea18f9e5cded2e9b00a6c66c4eb1f1be
ZH <zharov.2010@yandex.ru>
2022-12-26 19:18:49 +0300

    dz i task2

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,14 +1,19 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class Task2 {
 
-    //Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', определите,
+    //Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', 
+    //определите,
     // является ли входная строка логически правильной.
     // Входная строка логически правильная, если:
     // 1) Открытые скобки должны быть закрыты скобками того же типа.
-    // 2) Открытые скобки должны быть закрыты в правильном порядке. Каждая закрывающая скобка имеет соответствующую
+    // 2) Открытые скобки должны быть закрыты в правильном порядке.
+    // Каждая закрывающая скобка имеет соответствующую
     // открытую скобку того же типа.
     // ()[] = true
     // () = true
@@ -16,12 +21,65 @@ public class Task2 {
     // ()() = true
     // )()( = false
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        
+        Deque<Character> dq = new ArrayDeque<>(Arrays.asList('{','(',')','}'));
+        
+        
+        Task2 sq = new Task2();
+        System.out.println(sq.validate(dq));
     }
 
-    public boolean validate(Deque<Integer> deque){
+    public boolean validate(Deque<Character> dq) throws Exception{
 
-        return false;
-    }
+        if (dq == null)  {
+            throw new Exception("pusto");
+        }
+       
+        if (dq.size()%2 != 0 || dq.peekFirst() == ')' || dq.peekFirst() == '}' || dq.peekFirst() == ']'){
+            return false;
+        }
+
+        Character x = dq.pollFirst(); 
+        while (dq.size() > 0){
+            if (x == '('){
+                if (dq.peekFirst() == ')'){
+                    dq.pollFirst();
+                    x = dq.pollFirst();
+                }
+                else if (dq.peekLast() == ')'){
+                    dq.pollLast();
+                    x = dq.pollFirst();
+                }
+                else return false;
+            }
+            else if (x == '{'){
+                if (dq.peekFirst() == '}'){
+                    dq.pollFirst();
+                    x = dq.pollFirst();
+                }
+                else if (dq.peekLast() == '}'){
+                    dq.pollLast();
+                    x = dq.pollFirst();
+                }
+                else return false;
+            }
+            else if (x == '['){
+                if (dq.peekFirst() == ']'){
+                    dq.pollFirst();
+                    x = dq.pollFirst();
+                }
+                else if (dq.peekLast() == ']'){
+                    dq.pollLast();
+                    x = dq.pollFirst();
+                }
+                else return false;
+            }
+            else return false;
+        }
+        if (dq.size() == 0){
+            return true;
+        }
+        else return false;
+ }
 }
